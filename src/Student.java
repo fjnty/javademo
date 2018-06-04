0ec3c0d9e5457709fb1f346e3a9702a7d3a285de
傅嘉宁 <fjnty@hotmail.com>
2018-06-04 22:35:08 +0800

    The Java learning program

---
+++ src/Student.java
@@ -0,0 +1,38 @@
+public class Student {
+    private int id;
+    private String name;
+    private boolean male;
+    private double account;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isMale() {
+        return male;
+    }
+
+    public double getAccount() {
+        return account;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMale(boolean male) {
+        this.male = male;
+    }
+
+    public void setAccount(double account) {
+        this.account = account;
+    }
+}
